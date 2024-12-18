63c168fdb7abcb3a789ba354c8f2a82fb3cbb4ba
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-12-18 22:41:41 +0530

    added categories and new components section

--- src/app/components/ProductList.tsx
+++ src/app/components/ProductList.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const ProductList = () => {
     return (
         <div className='flex gap-x-8 gap-y-16 justify-between flex-wrap'>
-            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] md:w-[22%]'>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
                 <div className='relative w-full h-80'>
                     <Image src="https://images.pexels.com/photos/29525865/pexels-photo-29525865/free-photo-of-serene-winter-forest-path-in-tranquil-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
                         alt="" fill sizes='25vw' className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
@@ -22,7 +22,7 @@ const ProductList = () => {
                 <button className='rounded-2xl ring-1 ring-cartColor text-cartColor w-max py-2 px-4 text-xs hover:bg-cartColor hover:text-white'>Add to cart</button>
             </Link>
 
-            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] md:w-[22%]'>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
                 <div className='relative w-full h-80'>
                     <Image src="https://images.pexels.com/photos/29525865/pexels-photo-29525865/free-photo-of-serene-winter-forest-path-in-tranquil-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
                         alt="" fill sizes='25vw' className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
@@ -39,7 +39,7 @@ const ProductList = () => {
                 <button className='rounded-2xl ring-1 ring-cartColor text-cartColor w-max py-2 px-4 text-xs hover:bg-cartColor hover:text-white'>Add to cart</button>
             </Link>
 
-            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] md:w-[22%]'>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
                 <div className='relative w-full h-80'>
                     <Image src="https://images.pexels.com/photos/29525865/pexels-photo-29525865/free-photo-of-serene-winter-forest-path-in-tranquil-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
                         alt="" fill sizes='25vw' className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
@@ -56,7 +56,7 @@ const ProductList = () => {
                 <button className='rounded-2xl ring-1 ring-cartColor text-cartColor w-max py-2 px-4 text-xs hover:bg-cartColor hover:text-white'>Add to cart</button>
             </Link>
 
-            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] md:w-[22%]'>
+            <Link href="/test" className='w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]'>
                 <div className='relative w-full h-80'>
                     <Image src="https://images.pexels.com/photos/29525865/pexels-photo-29525865/free-photo-of-serene-winter-forest-path-in-tranquil-setting.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
                         alt="" fill sizes='25vw' className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
