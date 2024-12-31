aa9792a67fff930f0ce0c2312118c6e44ef2610a
revanthkashaboina <revanthkashaboina99@gmail.com>
2024-12-31 23:44:06 +0530

    added single page integration

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,43 +2,51 @@ import React from 'react'
 import ProductImages from '../components/ProductImages'
 import CustomizeProducts from '../components/CustomizeProducts'
 import Add from '../components/Add'
+import { wixClientServer } from '@/lib/wixClientServer'
+import { notFound } from 'next/navigation'
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+
+  const productSlug = params.slug;
+  const wixClient = wixClientServer();
+  const products = await wixClient.products.queryProducts().eq("slug", productSlug).find();
+
+  if (!products.items[0]) {
+    return notFound();
+  }
+
+  const product = products.items[0];
+  console.log(product);
+
   return (
     <div className='px-4 md:px-8  lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16'>
       {/* IMAGE */}
       <div className='w-full lg:w-1/2 lg:sticky top-20 h-max'>
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXT */}
       <div className='w-full lg:w-1/2 flex flex-col gap-6'>
-        <h1 className='text-4xl font-medium'>Product Name</h1>
-        <p className='text-gray-500'>Lorem ispisuf dolsr sjjt ekjakn ebfkje wkewkld vbw. Pjsabcb yackj ajnf wodxqlM viuawfn ehbv, CVKEJV. Infwine ioen iue, ociqnj</p>
+        <h1 className='text-4xl font-medium'>{product.name}</h1>
+        <p className='text-gray-500'>{product.description}</p>
         <div className='h-[2px] bg-gray-100' />
-        <div className='flex items-center gap-4'>
-          <h3 className='text-xl text-gray-500 line-through'>$59</h3>
-          <h2 className='font-medium text-2xl'>$49</h2>
-        </div>
+        {product.price?.price === product.price?.discountedPrice ? (
+          <h2 className='font-medium text-2xl'>${product.price?.price}</h2>
+        ) : (
+          <div className='flex items-center gap-4'>
+            <h3 className='text-xl text-gray-500 line-through'>${product.price?.price}</h3>
+            <h2 className='font-medium text-2xl'>${product.price?.discountedPrice}</h2>
+          </div>
+        )}
         <div className='h-[2px] bg-gray-100' />
         <CustomizeProducts />
         <Add />
         <div className='h-[2px] bg-gray-100' />
-        <div className='text-sm'>
-          <h4 className='font-medium mb-4'>Title</h4>
-          <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed? Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed?</p>
-        </div>
-        <div className='text-sm'>
-          <h4 className='font-medium mb-4'>Title</h4>
-          <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed? Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed?</p>
-        </div>
-        <div className='text-sm'>
-          <h4 className='font-medium mb-4'>Title</h4>
-          <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed? Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed?</p>
-        </div>
-        <div className='text-sm'>
-          <h4 className='font-medium mb-4'>Title</h4>
-          <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed? Quisquam, quae temporibus. Quibusdam dolorum facere, consequatur quidem, voluptatem quas illo voluptatum, quod eius sed?</p>
-        </div>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div className='text-sm' key={section.title}>
+            <h4 className='font-medium mb-4'>{section.title}</h4>
+            <p className='text-justify'>{section.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/app/components/ProductImages.tsx
+++ src/app/components/ProductImages.tsx
@@ -2,27 +2,27 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 
-const images = [
-  { id: 1, url: "https://images.pexels.com/photos/22669491/pexels-photo-22669491/free-photo-of-traditional-yellow-lanterns-on-tree.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
-  { id: 2, url: "https://images.pexels.com/photos/26606457/pexels-photo-26606457/free-photo-of-close-up-of-colorful-stars-christmas-decoration.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
-  { id: 3, url: "https://images.pexels.com/photos/19560861/pexels-photo-19560861/free-photo-of-historic-tram-28-on-a-narrow-alley-in-lisbon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
-  { id: 4, url: "https://images.pexels.com/photos/29803469/pexels-photo-29803469/free-photo-of-majestic-hight-tatras-mountains-in-slovakia-at-sunrise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" }
-]
+// const images = [
+//   { id: 1, url: "https://images.pexels.com/photos/22669491/pexels-photo-22669491/free-photo-of-traditional-yellow-lanterns-on-tree.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
+//   { id: 2, url: "https://images.pexels.com/photos/26606457/pexels-photo-26606457/free-photo-of-close-up-of-colorful-stars-christmas-decoration.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
+//   { id: 3, url: "https://images.pexels.com/photos/19560861/pexels-photo-19560861/free-photo-of-historic-tram-28-on-a-narrow-alley-in-lisbon.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" },
+//   { id: 4, url: "https://images.pexels.com/photos/29803469/pexels-photo-29803469/free-photo-of-majestic-hight-tatras-mountains-in-slovakia-at-sunrise.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" }
+// ]
 
-const ProductImages = () => {
+const ProductImages = ({items}:{items:any}) => {
 
   const [index, setIndex] = useState(0)
 
   return (
     <div className=''>
       <div className='h-[500px] relative'>
-        <Image src={images[index].url} alt="" fill sizes='50vw' className='object-cover rounded-md' />
+        <Image src={items[index].image?.url} alt="" fill sizes='50vw' className='object-cover rounded-md' />
       </div>
       <div className='flex justify-between gap-4 mt-8 '>
         {
-          images.map((image, i) => (
-            <div className='w-1/4 h-32 relative mt-8 cursor-pointer' key={image.id} onClick={()=> setIndex(i)}>
-              <Image src={image.url} alt="" fill sizes='30vw' className='object-cover rounded-md' />
+          items.map((item:any, i:number) => (
+            <div className='w-1/4 h-32 relative mt-8 cursor-pointer' key={item._id} onClick={()=> setIndex(i)}>
+              <Image src={item.image?.url} alt="" fill sizes='30vw' className='object-cover rounded-md' />
             </div>
           ))
         }
